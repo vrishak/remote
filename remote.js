3d893de2a02795b19d6fdc525196dde138841f55
Santhosh Koppula <sk185521@ncr.com>
2018-07-28 00:00:14 +0530

    asdf

--- remote.js
+++ remote.js
@@ -1,17 +1,15 @@
-var html = "<div class='container'>";
-  html += "<input/>";
-  html +="<div class='row'><a href='#'>1</a><a href='#'>2</a><a href='#'>3</a></div>";
-  html +="<div class='row'><a href='#'>4</a><a href='#'>5</a><a href='#'>6</a></div>";
-  html +="<div class='row'><a href='#'>7</a><a href='#'>8</a><a href='#'>9</a></div>";
-  html +="<div class='row'><a href='#'>*</a><a href='#'>0</a><a href='#'>#</a></div>";
-  html +="<div class='row'><a class='call' href='#'><i class='fa fa-phone'></i></a></div>";
-html +="</div>";
-
-
 
 $(document).ready(function(){
 	$("head").append("<link rel='stylesheet' href='https://rawgit.com/vrishak/remote/master/remote.css' type='text/css' media='screen'>");
-	$("body").innerHTML = html;
+	var html = "<div class='container'>";
+	  html += "<input/>";
+	  html +="<div class='row'><a href='#'>1</a><a href='#'>2</a><a href='#'>3</a></div>";
+	  html +="<div class='row'><a href='#'>4</a><a href='#'>5</a><a href='#'>6</a></div>";
+	  html +="<div class='row'><a href='#'>7</a><a href='#'>8</a><a href='#'>9</a></div>";
+	  html +="<div class='row'><a href='#'>On/Off</a><a href='#'>0</a><a href='#'><<</a></div>";
+	  html +="<div class='row'><a class='call' href='#'>OK</a></div>";
+	  html +="</div>";
+	document.body.innerHTML = html;
 	
 	$('.row a').on('click', function() {
 	var $store = $('input'),
