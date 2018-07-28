42284ded299371880c7e2f9b6400379a5141b84f
Santhosh Koppula <sk185521@ncr.com>
2018-07-28 12:41:33 +0530

    adfs

--- remote.js
+++ remote.js
@@ -1,12 +1,17 @@
 
 $(document).ready(function(){
-	$("head").append("<link rel='stylesheet' href='https://rawgit.com/vrishak/remote/master/remote.css' type='text/css' media='screen'>");
+	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+		$("head").append("<link rel='stylesheet' href='https://rawgit.com/vrishak/remote/master/m_remote.css' type='text/css' media='screen'>");
+    } else {
+		$("head").append("<link rel='stylesheet' href='https://rawgit.com/vrishak/remote/master/remote.css' type='text/css' media='screen'>");
+	}
 	var html = "<div class='container'>";
 	  html += "<input/>";
 	  html +="<div class='row'><a href='#'>1</a><a href='#'>2</a><a href='#'>3</a></div>";
 	  html +="<div class='row'><a href='#'>4</a><a href='#'>5</a><a href='#'>6</a></div>";
 	  html +="<div class='row'><a href='#'>7</a><a href='#'>8</a><a href='#'>9</a></div>";
-	  html +="<div class='row'><a href='#'><span style='color:red'><strong>O</strong></span></a><a href='#'>0</a><a href='#'><<</a></div>";
+	  html +="<div class='row'><a href='#'>CH-</a><a href='#'>0</a><a href='#'>CH+</a></div>";
+      html +="<div class='row'><a href='#'>V-</a><a href='#'><span style='color:red'><strong>O</strong></span></a><a href='#'>V+</a></div>";
 	  html +="<div class='row'><a class='call' href='#'>OK</a></div>";
 	  html +="</div>";
 	document.body.innerHTML = html;
@@ -15,11 +20,8 @@ $(document).ready(function(){
 	var $store = $('input'),
 			$number = $('.call'),			
 			dialed = $(this).text();
-	if(dialed == "OK") {
-		submitCodes($store.val());
-		$store.val("");
-	} else if(dialed == "O") {
-		submitCodes("O");
+	if(dialed == "O" || dialed == "OK" || dialed == "V+" || dialed == "V-" || dialed == "CH+" || dialed == "CH-") {
+		submitCodes(dialed);
 		$store.val("");
 	} else if(dialed == "<<") {
 		$store.val($store.val().substring(0,$store.val().length-1));
@@ -32,7 +34,15 @@ $(document).ready(function(){
 	function submitCodes(codes) {
 		for (var i = 0; i < codes.length; i++) {
 			var ch = codes.charAt(i);
-			if(ch == "O") {
+			if(ch == "CH+") {
+				send(12582944);
+			} else if(ch == "CH-") {
+				send(12582945);
+			} else if(ch == "V+") {
+				send(12582928);
+			} else if(ch == "V-") {
+				send(12582929);
+			} else  if(ch == "O") {
 				send(12582924);
 			} else if(ch == "0") {
 				send(12582912);
